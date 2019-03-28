b9cc440b2170a8139f51e072cbe468e727b6e62f
Andrey Galchevski <andreygalchevski@gmail.com>
2019-03-28 18:51:07 +0200

    add eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <h1>Hello, world!</h1>
-          <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
